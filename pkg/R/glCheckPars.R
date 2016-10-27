0c2ca84ba40566bd8a10c0459c32f94570921180
dscott <dscott@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-27 11:02:52 +0000

    Made working version of package except for dgl
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/genlambdadist@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glCheckPars.R
+++ pkg/R/glCheckPars.R
@@ -1,5 +1,7 @@
+
 glCheckPars <- function(param, version = "FMKL", lambda5 = NULL)
 {
+
   switch(version,
          ## allows for alternate expressions
          freimer = ,
@@ -30,31 +32,27 @@ glCheckPars <- function(param, version = "FMKL", lambda5 = NULL)
 }
 
 
-
-
-glCheckParsErrorMessage  <- function(param, version){
-  paste("The parameter values", paste(param, collapse = " "),
-        "\ndo not produce a proper distribution for the", version,
-        "GLD version \n - see documentation for glCheckPars")
-}
-
 glCheckParsFMKL <- function(param)
 {
+  errMessage <- ""
+  case <- TRUE
   lambda4 <- param[4]
   lambda3 <- param[3]
   lambda2 <- param[2]
   lambda1 <- param[1]
   if (lambda2 <= 0) {
-    return(FALSE)
-  } else {
-    return(TRUE)
+    case <- "error"
+    errMessage <- "lambda2 must be greater than or equal to zero"
   }
-
+  result <- list(case = case, errMessage = errMessage)
+  return(result)
 }
 
 
 glCheckParsRS <- function(param)
 {
+  errMessage <- ""
+  case <- TRUE
   lambda4 <- param[4]
   lambda3 <- param[3]
   lambda2 <- param[2]
@@ -63,113 +61,140 @@ glCheckParsRS <- function(param)
   if (lambda3 * lambda4 > 0) {
     if ((lambda3 > 0) & (lambda4 > 0)) {
       if (lambda2 <= 0) {
-        ret <- FALSE
+        case <- "error"
+        errMessage <- paste("lambda2 must be postive when lambda3",
+                            "and lambda4 > 0")
       } else {
-        ret <- TRUE
+        case <- TRUE
       }
     }
     if ((lambda3 < 0) & (lambda4 < 0)) {
       if (lambda2 >= 0) {
-        ret <- FALSE
+        case <- "error"
+        errMessage <- paste("lambda2 must be negative when lambda3",
+                            "and lambda4 < 0")
       } else {
-        ret <- TRUE
+        case <- TRUE
       }
     }
   } else {
     if (lambda2 >= 0) {
-      return(FALSE)
+      case <- "error"
+      errMessage <- paste("lambda2 must be greater than or equal",
+                          "to zero when lambda3*lambda4 >= 0")
     }
     if ((lambda3 > 0) & (lambda3 < 1) & (lambda4 < 0)) {
-      return(FALSE)
+      case <- "error"
+      errMessage <- paste("when lambda3 is between 0 and 1",
+                          "lambda4 must not be negative")
     }
     if ((lambda4 > 0) & (lambda4 < 1) & (lambda3 < 0)) {
-      return(FALSE)
+      case <- "error"
+      errMessage <- paste("when lambda4 is between 0 and 1",
+                          "and lambda3 must not be a negative")
     }
     lc <- lambda3
     ld <- lambda4
     if ((lambda3 > -1) & (lambda3 < 0) & (lambda4 > 1)) {
       if (((1 - lc)^(1 - lc)*(ld - 1)^(ld - 1))/
           ((ld - lc)^(ld - lc)) > -lc/ld) {
-        return(FALSE)
+        case <- "error"
       } else {
-        return(TRUE)
+        case <- TRUE
       }
     }
     if ((lambda4 > -1) & (lambda4 < 0) & (lambda3 > 1)) {
       if (((1 - ld)^(1 - ld)*(lc - 1)^(lc - 1))/
           ((lc - ld)^(lc - ld)) > -ld/lc) {
-        return(FALSE)
+        case <- "error"
       } else {
-        return(TRUE)
+        case <- TRUE
       }
     }
-    if (lambda3 == 0) {
+    if (isTRUE(all.equal(lambda3,0))) {
       if (lambda4 > 0) {
         if (lambda2 < 0) {
-          return(FALSE)
+          case <- "error"
+          errMessage <- paste("when lambda3 is equal to zero",
+          "and lambda4 is postive,lambda2 must be negative")
         }
-        ret <- TRUE
+        case <- TRUE
       }
-      if (lambda4 == 0) {
-        warning("RS GLD version: lambda3 and lambda4
-                zero gives a point mass at lambda1")
+      if (isTRUE(all.equal(lambda4,0))) {
+        case <- "error"
+        errMessage <- paste("lambda3 and lambda4 zero gives a",
+        "point mass at lambda1")
+
       }
       if (lambda4 < 0) {
         if (lambda2 > 0) {
-          return(FALSE)
+          case <- "error"
+          errMessage <- paste("when lambda3 is equal to zero and",
+          "lambda4 is negative, lambda2 must be postive")
         }
-        ret <- TRUE
+        case <- TRUE
       }
     }
-    if (lambda4 == 0) {
+    if (isTRUE(all.equal(lambda4,0))) {
       if (lambda3 > 0) {
         if (lambda2 < 0) {
-          return(FALSE)
+          case <- "error"
+          errMessage <- paste("when lambda3 is postive and lambda4",
+          "is zero, lambda2 must be negative")
         }
-        ret <- TRUE
+        case <- TRUE
       }
-      if (lambda3 == 0) {
-        warning("RS GLD version: lambda3 and lambda4
-                zero gives a point mass at lambda1")
+      if (isTRUE(all.equal(lambda3,0))) {
+        case <- "error"
+        errMessage <- paste("lambda3 and lambda4 zero gives a point",
+        "mass at lambda1")
       }
       if (lambda3 < 0) {
         if (lambda2 > 0) {
-          return(FALSE)
+          case <- "error"
+          errMessage <- paste("when lambda3 is negative and lambda4",
+          "is zero,lambda2 must be postive")
         }
-        ret <- TRUE
+        case <- TRUE
       }
     }
-    if (is.null(ret)) {
+    if (is.null(case)) {
         warning("RS param return not set:
                 please email maintainer with example")
-        ret <- TRUE}
+        case <- TRUE}
   }
-  ret
+  result <- list(case = case, errMessage = errMessage)
+  return(result)
 }
 
 
 glCheckParsVSK <- function(param)
 {
+  errMessage <- ""
+  case <- TRUE
   lambda4 <- param[4]
   lambda3 <- param[3]
   lambda2 <- param[2]
   lambda1 <- param[1]
 
   if (lambda2 <= 0) {
-    ret <- FALSE
-    warning("Negative or zero beta")
+    case <- "error"
+    errMessage <- "lambda2 must be greater or equal to zero"
+
   } else {  # delta check
     if ((lambda3 < 0) | (lambda3 > 1)) {
-      ret <- FALSE
-    } else {
-      ret <- TRUE
+      case <- "error"
+      errMessage <- "lambda3 must between 0 and 1"
     }
   }
-  ret
+  result <- list(case = case, errMessage = errMessage)
+  return(result)
 }
 
 glCheckParsAS <- function(param)
 {
+  errMessage <- ""
+  case <- TRUE
   lambda4 <- param[4]
   lambda3 <- param[3]
   lambda2 <- param[2]
@@ -180,6 +205,8 @@ glCheckParsAS <- function(param)
 
 glCheckParsFMKL5 <- function(param, lambda5)
 {
+  errMessage <- ""
+  case <- TRUE
   lambda4 <- param[4]
   lambda3 <- param[3]
   lambda2 <- param[2]
@@ -187,6 +214,7 @@ glCheckParsFMKL5 <- function(param, lambda5)
 
   stop("glCheckParsFMKL5 not yet implemented")
 }
+
 
 
 
